637ac96f0d638c45b918b987018c4e91b4e7fa5c
Babar Ali <232048@students.au.edu.pk>
2026-09-21 15:22:10 +0500

    Add user input to hello.py

--- hello.py
+++ hello.py
@@ -3,11 +3,3 @@ print("Hello GitHub!")
 name = input("What is your name? ")
 
 print("Hello", name)
-
-print("This is my third commit!")
-
-print("This line was added locally.")
-
-print("This line was added directly on GitHub.")
-
-print("Hello from GitHub Desktop!")
